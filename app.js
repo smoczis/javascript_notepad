737e9eb66dd523ec65dd45bf7c0307ca5c510171
smoczis <romanowski.mateusz93@gmail.com>
2018-04-11 22:35:17 +0200

    Taks 2 - add new city to dynamically created list

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var addBtn = document.querySelector(".add-employee-btn");
+var list = document.createElement("ul");
+var wrapper = document.getElementById("wrapper");
+wrapper.appendChild(list);
+var listElement = document.createElement("li");
+list.appendChild(listElement);
+listElement.innerHTML = "Paryż";
+
+var addBtn = document.createElement("button");
+addBtn.innerHTML = "Create new element";
+wrapper.appendChild(addBtn);
+
 addBtn.addEventListener("click", function () {
-    var li = document.createElement("li");
-    var list = document.querySelector(".employee-list");
-    var input = document.querySelector(".employee-name-input");
-    li.innerHTML = input.value;
-    list.appendChild(li);
+    var newListElement = document.createElement("li");
+    list.appendChild(newListElement);
+    newListElement.innerHTML = "New City";
 }, false);
+
+
+
+
+
+
+console.log(listElement);
+console.log(wrapper);
+console.log(list);
